42114fd714a64227d1a74201e9d8339717adad3b
Jaclyn French <77642588+jaclynmariefrench@users.noreply.github.com>
2021-09-19 11:59:58 -0500

    still working on the backend

--- src/components/movieTv/MovieTvList.js
+++ src/components/movieTv/MovieTvList.js
@@ -13,15 +13,22 @@ export const MovieTvList = () => {
         <>
             <header>List of Spooktober Movies</header>
             <article className="movie-list">
-                {movieTvs.map((movie) => {
-                    return (
-                        <section key={`movie--${movie.id}`} className="movie">
-                            <div className="movie__title">
-                                <h3>{movie.title}</h3> 
-                            </div>
-                        </section>
-          );
-        })}
+            {movieTvs.map((m) => {
+            return (
+            <section key={m.id} className="add-to-watch">
+                <h3 className="add-cal">{m.title}</h3>
+                    {
+                            m.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(m.id)}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(m.id)}
+                                    >Join</button>
+                        }
+                </section>
+        );
+              })}
             </article>
         </>
     )
