2c6e4ef2632444ba713992a0f5ab7b6f14543e05
Jaclyn French <77642588+jaclynmariefrench@users.noreply.github.com>
2021-09-21 08:45:39 -0500

    so close to getting the dropdowns to work

--- src/components/search/searchForm.js
+++ src/components/search/searchForm.js
@@ -1,120 +1,115 @@
-import { useContext, useEffect, useState } from "react"
-import { MovieTvContext } from "../movieTv/MovieTvProvider"
-import { SearchContext } from "../search/searchProvider"
+import { useContext, useEffect, useState } from "react";
+import { MovieTvContext } from "../movieTv/MovieTvProvider";
+import { SearchContext } from "../search/searchProvider";
 
 export const SimpleSearch = () => {
-    const [searchTerm, setSearchTerm ] = useState("")
-    const [eraSelect, setEra ] = useState({id: 0})
-    const [spiritSelect, setSpirit ] = useState({id:0})
-    
-    const { movieTvs, getMovieTv, leaveWaitlist, addWaitlist } = useContext(MovieTvContext)
-    const { getEras, eraList, spiritList, getSpirits } = useContext(SearchContext)
+  const [searchTerm, setSearchTerm] = useState("");
+  const [eraSelect, setEra] = useState({ id: 0 });
+  const [spiritSelect, setSpirit] = useState({ id: 0 });
+  const [searchedMovies, setSearchedMovies] = useState([]);
 
-    useEffect(()=> {
-        getMovieTv();
-    }, [])
+  const { movieTvs, getMovieTv, leaveWaitlist, addWaitlist } =
+    useContext(MovieTvContext);
+  const { getEras, eraList, spiritList, getSpirits } =
+    useContext(SearchContext);
 
-    useEffect(()=> {
-        getEras();
-    }, [])
+  useEffect(() => {
+    getMovieTv();
+  }, []);
 
-    useEffect(()=> {
-        getSpirits();
-    }, [])
+  useEffect(() => {
+    getEras();
+  }, []);
 
-    useEffect(()=> {
-        let filteredMovie = []
-          for (const movie of movieTvs) {
-            for (const era of eraList) {
-              if(movie.era === era.id){
-                filteredMovie.push(movie)
-              }
-            }
-          }
-        setEra(filteredMovie)
-      }, [])
-  
+  useEffect(() => {
+    getSpirits();
+  }, []);
 
-    return (
-        <div className="simple-search">
-            <input
-                type="text"
-                placeholder="what vibe you want?"
-                onChange={(e) => {
-                    setSearchTerm(e.target.value)
-                }}
-            />
-            {/* DROPDOWN TOWN */}
-            <fieldset className="search_form">
-            <h2 className="dropdown__title">What's the Era?</h2>
-                <div className="form-group">
-                <select
-                    onChange={(e)=> {
-                        setEra(e.target.value)
-                    }}
+  useEffect(() => {
+    if (searchTerm !== "" || eraSelect > 0 || spiritSelect > 0) {
+      const searchedTacos = movieTvs.filter((movie) =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        movie.genre.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        movie.subGenre.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        movie.era.id == eraSelect || 
+        movie.spirit.id == spiritSelect
+      );
+      setSearchedMovies(searchedTacos);
+    } else {
+      setSearchedMovies(movieTvs);
+    }
+  }, [searchTerm, eraSelect, spiritSelect]);
+
+  return (
+    <div className="simple-search">
+      <input
+        type="text"
+        placeholder="what vibe you want?"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      {/* DROPDOWN TOWN */}
+      <fieldset className="search_form">
+        <h2 className="dropdown__title">What's the Era?</h2>
+        <div className="form-group">
+          <select
+            onChange={(e) => {
+              setEra(e.target.value);
+            }}
+          >
+            <option value="0">Select the Era</option>
+            {eraList.map((m) => (
+              <option key={m.id} value={m.id}>
+                {m.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </fieldset>
+      <fieldset className="search_form">
+        <h2 className="dropdown__title">What's the Spirit?</h2>
+        <div className="form-group">
+          <select
+            onChange={(e) => {
+              setSpirit(e.target.value);
+            }}
+          >
+            <option value="0">Select the Spirit</option>
+            {spiritList.map((m) => (
+              <option key={m.id} value={m.id}>
+                {m.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </fieldset>
+      <div>
+        {/* RESULTS */}
+
+        {searchedMovies.map((value, key) => {
+          return (
+            <section key={key} className="add-to-watch">
+              <h3 className="add-cal">{value.title}</h3>
+              {value.added ? (
+                <button
+                  className="btn btn-3"
+                  onClick={() => leaveWaitlist(value.id)}
                 >
-                    <option value="8">Select the Era</option>
-                    {eraList.map((m) => (
-                    <option key={m.id} value={m.id}>
-                        {m.label}
-                    </option>
-                    ))}
-                </select>
-                </div>
-            </fieldset>
-            <fieldset className="search_form">
-            <h2 className="dropdown__title">What's the Spirit?</h2>
-                <div className="form-group">
-                <select
-                    onChange={(e)=> {
-                        setSpirit(e.target.value)
-                    }}
+                  Leave
+                </button>
+              ) : (
+                <button
+                  className="btn btn-2"
+                  onClick={() => addWaitlist(value.id)}
                 >
-                    <option value="8">Select the Spirit</option>
-                    {spiritList.map((m) => (
-                    <option key={m.id} value={m.id}>
-                        {m.label}
-                    </option>
-                    ))}
-                </select>
-                </div>
-            </fieldset>
-                <div>
-                {/* RESULTS */}
-                {
-                    movieTvs.filter(
-                        (value)=> {
-                            if (searchTerm == "") {
-                                return value
-                            } else if (value.title.toLowerCase().includes(searchTerm.toLowerCase())) 
-                            {
-                                return value
-                            } else if (value.genre.toLowerCase().includes(searchTerm.toLowerCase())) 
-                            {
-                                return value
-                            } else if (value.subGenre.toLowerCase().includes(searchTerm.toLowerCase())) 
-                            {
-                                return value
-                            }       
-                        }).map(
-                            (value, key)=> {
-                                return(
-                                    <section key={key} className="add-to-watch">
-                                    <h3 className="add-cal">{value.title}</h3>
-                                        {
-                                                value.added
-                                                    ? <button className="btn btn-3"
-                                                        onClick={() => leaveWaitlist(value.id)}
-                                                        >Leave</button>
-                                                    : <button className="btn btn-2"
-                                                        onClick={() => addWaitlist(value.id)}
-                                                        >Join</button>
-                                            }
-                                        </section>
-                                )
-                            }
-                        )
-                            }
-
-                </div>
-            </div>)}
+                  Join
+                </button>
+              )}
+            </section>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
